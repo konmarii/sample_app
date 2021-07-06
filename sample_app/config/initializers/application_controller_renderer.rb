91ca279db04a484f45638d7d51cea61d1b128731
EC2 Default User <ec2-user@ip-172-31-73-126.ec2.internal>
2021-07-06 02:54:43 +0000

    [finish]1-7

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '37a607baf81d74bd15e626a2fd06f5a3f0ce361610a133c5b581d7db5e000daaff8637bf2233df7254bc356e9336beb173153af3b9d6bc6a79eb8e1d3be1ee6e'
---
+++ sample_app/db/migrate/20210706002738_addmageld_to_list.rb
@@ -0,0 +1,4 @@
+class AddmageldToList < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
